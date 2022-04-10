8ca75c181e43140e55d7b7d8f9f862ff6aad2a88
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-10 18:17:32 -0400

    Update project2.s
    beginig to create a function for leading and trailing blank characters. establishing the counter which will iterate through the different branches of the code.

--- project2.s
+++ project2.s
@@ -18,3 +18,13 @@ li $t4, 26 # loading the number 26 to register t4
 add $t5,$t4,$t3 # adding the modulo of my id and 26 and storing it in the t5 register
 li $t6, 10 # loading the number 10 to the t6 register
 sub $t1,$t5,$t6 # loading the value of N-10 to register t7
+
+# remove leading and trailing blackspace characters and tab characters (BS = 8, HT = 9, VT = 11, S-32) similar to Trim(char[])
+
+begin:
+# lb $t2, $t0($a0)
+# changing the syntax of this line 
+add $t2, $t0, $a0
+lb $t2, ($t2)
+# iterate the counter
+addi $t0,$t0,1 
