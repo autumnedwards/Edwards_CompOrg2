f18e4c58c531fc1a55b9c1003626d15b96524739
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-10 18:14:03 -0400

    Update project2.s
    using the old project to calculate my X, M and N

--- project2.s
+++ project2.s
@@ -8,3 +8,8 @@ addi $v0, $0, 8 # system call code for reading a string in MIPS is 8
 la $a0, userInput # a0 register set to the location in memory to which the computer will record the input
 li $a1, 1000 # i want to read 1000 characters 
 syscall 
+
+# finding X, M and N
+li, $t1,2992633 # loading my id to the register t1
+li, $t2, 11 # loading the number 11 to the register t2
+div $t1,$t2 # dividing my id number by the number 11
