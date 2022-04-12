54ee8b2504e1a944df221c2603c1837b0c0893be
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-11 21:54:58 -0400

    Update project2.s
    changing t3 to t0 bc of the error

--- project2.s
+++ project2.s
@@ -81,10 +81,10 @@ beq $t0,11,trailingCharacters
 beq $t0,10,printSum
 beq $t0,0,printSum
 
-ble $t3,$t5,Pnumber #if the value of t2 is less than or equal to 57 go to possible number
-blt $t3,$t7,Puppercase #if the value of t2 is less than 87 or equal to go to possible uppercase
-blt $t3,$t9,Plowercase #if the value of t2 is less than or equal to 119 go to possible lowercase
-bge $t3, $t9, invalid #if the value of t2 is greater than 118 go to return zero
+ble $t0,$t5,Pnumber #if the value of t2 is less than or equal to 57 go to possible number
+blt $t0,$t7,Puppercase #if the value of t2 is less than 87 or equal to go to possible uppercase
+blt $t0,$t9,Plowercase #if the value of t2 is less than or equal to 119 go to possible lowercase
+bge $t0, $t9, invalid #if the value of t2 is greater than 118 go to return zero
 
 Pnumber:
 bge $t3,$s4,integer
